074ec80997d48898d8329452996871939171476a
JTB <jtbdevelopment@gmail.com>
2016-10-08 13:42:13 -0400

    fixed injection for tests

--- dist/core-games-bootstrap-ui.js
+++ dist/core-games-bootstrap-ui.js
@@ -353,158 +353,166 @@ angular.module('coreGamesBootstrapUi.services').factory('jtbBootstrapGameActions
 
 'use strict';
 
-angular.module('coreGamesBootstrapUi.templates').run(function ($templateCache) {
-    $templateCache.put('views/core-bs/admin/admin-stats.html', '' +
-        '<div class="admin-stats"><div class="row">' +
-        '<table class="table table-striped table-condensed table-admin-states"> ' +
-        '<tr> ' +
-        '<th></th> ' +
-        '<th class="text-right">Last 24 hours</th> ' +
-        '<th class="text-right">Last 7 days</th> ' +
-        '<th class="text-right">Last 30 days</th> ' +
-        '</tr> ' +
-        '<tr class="text-right"> ' +
-        '<th class="text-left">Total Games</th> ' +
-        '<td colspan="3">{{admin.gameCount}}</td> ' +
-        '</tr> ' +
-        '<tr class="text-right"> ' +
-        '<th class="text-left">Games Created</th> ' +
-        '<td>{{admin.gamesLast24hours}}</td> ' +
-        '<td>{{admin.gamesLast7days}}</td> ' +
-        '<td>{{admin.gamesLast30days}}</td> ' +
-        '</tr> ' +
-        '<tr class="text-right"> ' +
-        '<th class="text-left">Total Players</th> ' +
-        '<td colspan="3">{{admin.playerCount}}</td> ' +
-        '</tr> ' +
-        '<tr class="text-right"> ' +
-        '<th class="text-left">Players Created</th> ' +
-        '<td>{{admin.playersCreated24hours}}</td> ' +
-        '<td>{{admin.playersCreated7days}}</td> ' +
-        '<td>{{admin.playersCreated30days}}</td> ' +
-        '</tr> ' +
-        '<tr class="text-right"> ' +
-        '<th class="text-left">Players Logged In</th> ' +
-        '<td>{{admin.playersLastLogin24hours}}</td> ' +
-        '<td>{{admin.playersLastLogin7days}}</td> ' +
-        '<td>{{admin.playersLastLogin30days}}</td> ' +
-        '</tr> ' +
-        '</table> ' +
-        '</div> ' +
-        '</div>');
-    $templateCache.put('views/core-bs/admin/admin-switch-player.html', '' +
-        '<div class="admin-user">' +
-        '<div class="row">' +
-        '<div class="col-xs-4">' +
-        '<button class="btn btn-default btn-default-focus btn-success btn-stop-simulating"' +
-        'ng-disabled="!admin.revertEnabled" ng-click="admin.revertToNormal()">Stop</button>' +
-        '</div>' +
-        '<div class="col-xs-8">{{admin.revertText}}</div>' +
-        '</div>' +
-        '<div class="row">' +
-        '<div class="col-xs-4">' +
-        '<button class="btn btn-default btn-default-focus btn-primary btn-get-users"' +
-        'ng-click="admin.refreshData()">Get Users</button>' +
-        '</div>' +
-        '<div class="col-xs-8">' +
-        '<input class="form-control" type="text" ng-model="admin.searchText"' +
-        'placeholder="And Name contains..">' +
-        '</div>' +
-        '</div>' +
-        '<div class="row">' +
-        '<div class="col-xs-12">' +
-        '<table class="table table-striped table-condensed table-admin-users">' +
-        '<tr>' +
-        '<th>Display Name</th>' +
-        '<th>ID</th>' +
-        '<th>Switch</th>' +
-        '</tr>' +
-        '<tr ng-repeat="player in admin.players">' +
-        '<td>{{player.displayName}}</td>' +
-        '<td>{{player.id}}</td>' +
-        '<td>' +
-        '<button class="btn btn-danger btn-danger btn-change-user"' +
-        'ng-click="admin.switchToPlayer(player.id)">Switch to this user..</button>' +
-        '</td>' +
-        '</tr>' +
-        '</table>' +
-        '</div>' +
-        '</div>' +
-        '<div class="row">' +
-        '<div class="col-xs-12">' +
-        '<ul uib-pagination num-pages="admin.numberOfPages" items-per-page="admin.pageSize"' +
-        'direction-links="false" total-items="admin.totalItems" ng-model="admin.currentPage"' +
-        'ng-change="admin.changePage()"></ul>' +
-        '</div>' +
-        '</div>' +
-        '</div>'
-    );
-    console.log('running');
-    $templateCache.put('views/core-bs/admin/admin.html', '' +
-        '<div class="admin-screen">' +
-        '<div class="row"><div class="col-xs-8 col-xs-offset-2">' +
-        '<uib-tabset type="tabs" justified="true">' +
-        '<uib-tab heading="Stats">' +
-        '<div ng-include="\'views/core-bs/admin/admin-stats.html\'"></div>' +
-        '</uib-tab><uib-tab heading="Switch player">' +
-        '<div ng-include="\'views/core-bs/admin/admin-switch-player.html\'">' +
-        '</div>' +
-        '</uib-tab></uib-tabset></div></div></div>');
-});
+angular.module('coreGamesBootstrapUi.templates').run(
+    ['$templateCache',
+        function ($templateCache) {
+            $templateCache.put('views/core-bs/admin/admin-stats.html', '' +
+                '<div class="admin-stats"><div class="row">' +
+                '<table class="table table-striped table-condensed table-admin-states"> ' +
+                '<tr> ' +
+                '<th></th> ' +
+                '<th class="text-right">Last 24 hours</th> ' +
+                '<th class="text-right">Last 7 days</th> ' +
+                '<th class="text-right">Last 30 days</th> ' +
+                '</tr> ' +
+                '<tr class="text-right"> ' +
+                '<th class="text-left">Total Games</th> ' +
+                '<td colspan="3">{{admin.gameCount}}</td> ' +
+                '</tr> ' +
+                '<tr class="text-right"> ' +
+                '<th class="text-left">Games Created</th> ' +
+                '<td>{{admin.gamesLast24hours}}</td> ' +
+                '<td>{{admin.gamesLast7days}}</td> ' +
+                '<td>{{admin.gamesLast30days}}</td> ' +
+                '</tr> ' +
+                '<tr class="text-right"> ' +
+                '<th class="text-left">Total Players</th> ' +
+                '<td colspan="3">{{admin.playerCount}}</td> ' +
+                '</tr> ' +
+                '<tr class="text-right"> ' +
+                '<th class="text-left">Players Created</th> ' +
+                '<td>{{admin.playersCreated24hours}}</td> ' +
+                '<td>{{admin.playersCreated7days}}</td> ' +
+                '<td>{{admin.playersCreated30days}}</td> ' +
+                '</tr> ' +
+                '<tr class="text-right"> ' +
+                '<th class="text-left">Players Logged In</th> ' +
+                '<td>{{admin.playersLastLogin24hours}}</td> ' +
+                '<td>{{admin.playersLastLogin7days}}</td> ' +
+                '<td>{{admin.playersLastLogin30days}}</td> ' +
+                '</tr> ' +
+                '</table> ' +
+                '</div> ' +
+                '</div>');
+            $templateCache.put('views/core-bs/admin/admin-switch-player.html', '' +
+                '<div class="admin-user">' +
+                '<div class="row">' +
+                '<div class="col-xs-4">' +
+                '<button class="btn btn-default btn-default-focus btn-success btn-stop-simulating"' +
+                'ng-disabled="!admin.revertEnabled" ng-click="admin.revertToNormal()">Stop</button>' +
+                '</div>' +
+                '<div class="col-xs-8">{{admin.revertText}}</div>' +
+                '</div>' +
+                '<div class="row">' +
+                '<div class="col-xs-4">' +
+                '<button class="btn btn-default btn-default-focus btn-primary btn-get-users"' +
+                'ng-click="admin.refreshData()">Get Users</button>' +
+                '</div>' +
+                '<div class="col-xs-8">' +
+                '<input class="form-control" type="text" ng-model="admin.searchText"' +
+                'placeholder="And Name contains..">' +
+                '</div>' +
+                '</div>' +
+                '<div class="row">' +
+                '<div class="col-xs-12">' +
+                '<table class="table table-striped table-condensed table-admin-users">' +
+                '<tr>' +
+                '<th>Display Name</th>' +
+                '<th>ID</th>' +
+                '<th>Switch</th>' +
+                '</tr>' +
+                '<tr ng-repeat="player in admin.players">' +
+                '<td>{{player.displayName}}</td>' +
+                '<td>{{player.id}}</td>' +
+                '<td>' +
+                '<button class="btn btn-danger btn-danger btn-change-user"' +
+                'ng-click="admin.switchToPlayer(player.id)">Switch to this user..</button>' +
+                '</td>' +
+                '</tr>' +
+                '</table>' +
+                '</div>' +
+                '</div>' +
+                '<div class="row">' +
+                '<div class="col-xs-12">' +
+                '<ul uib-pagination num-pages="admin.numberOfPages" items-per-page="admin.pageSize"' +
+                'direction-links="false" total-items="admin.totalItems" ng-model="admin.currentPage"' +
+                'ng-change="admin.changePage()"></ul>' +
+                '</div>' +
+                '</div>' +
+                '</div>'
+            );
+            console.log('running');
+            $templateCache.put('views/core-bs/admin/admin.html', '' +
+                '<div class="admin-screen">' +
+                '<div class="row"><div class="col-xs-8 col-xs-offset-2">' +
+                '<uib-tabset type="tabs" justified="true">' +
+                '<uib-tab heading="Stats">' +
+                '<div ng-include="\'views/core-bs/admin/admin-stats.html\'"></div>' +
+                '</uib-tab><uib-tab heading="Switch player">' +
+                '<div ng-include="\'views/core-bs/admin/admin-switch-player.html\'">' +
+                '</div>' +
+                '</uib-tab></uib-tabset></div></div></div>');
+        }
+    ]
+);
 'use strict';
 
-angular.module('coreGamesBootstrapUi.templates').run(function ($templateCache) {
-    $templateCache.put('views/core-bs/sign-in/sign-in.html', '' +
-        '<div class="sign-in ng-cloak">' +
-        '<div class="container-fluid">' +
-        '<div class="row text-center">' +
-        '<div class="center">' +
-        '<p>{{signIn.message}}</p>' +
-        '</div>' +
-        '</div>' +
-        '</div>' +
-        '<div class="container-fluid" ng-show="signIn.showFacebook">' +
-        '<div class="row text-center">' +
-        '<div class="center">' +
-        '<a href="#" ng-click="signIn.fbLogin()"><img ng-src="/images/sign-in-with-facebook.png"/></a>' +
-        '</div>' +
-        '</div>' +
-        '</div>' +
-        '<div class="container-fluid" ng-show="signIn.showManual">' +
-        '<div class="row text-center">' +
-        '<p>Don\'t want to login with Facebook?</p>' +
-        '<form id="signin" action="/signin/authenticate" method="post" class="form-group">' +
-        '<input id="_csrf" name="_csrf" type="hidden" value="{{csrf}}"/>' +
-        '<div class="form-group">' +
-        '<label for="username">Username</label>' +
-        '<input id="username" name="username" type="text" size="25"/>' +
-        '</div>' +
-        '<div class="form-group">' +
-        '<label for="password">Password</label>' +
-        '<input id="password" name="password" type="password" size="25"/>' +
-        '</div>' +
-        '<div class="form-group">' +
-        '<div class="checkbox">' +
-        '<label>' +
-        '<input type="checkbox" id="remember-me" name="remember-me"> Remember me' +
-        '</label>' +
-        '</div>' +
-        '</div>' +
-        '<div class="form-group">' +
-        '<button type="submit" class="btn btn-default">Sign in</button>' +
-        '</div>' +
-        '</form>' +
-        '</div>' +
-        '</div>' +
-        '</div>'
-    );
-    $templateCache.put('views/core-bs/sign-in/signed-in.html', '' +
-        '<div class="row signed-in">' +
-        '<div class="row text-center">' +
-        '<div class="center">' +
-        '<p>Login successful...</p>' +
-        '</div>' +
-        '</div>' +
-        '</div>'
-    );
-});
+angular.module('coreGamesBootstrapUi.templates').run(
+    ['$templateCache',
+        function ($templateCache) {
+            $templateCache.put('views/core-bs/sign-in/sign-in.html', '' +
+                '<div class="sign-in ng-cloak">' +
+                '<div class="container-fluid">' +
+                '<div class="row text-center">' +
+                '<div class="center">' +
+                '<p>{{signIn.message}}</p>' +
+                '</div>' +
+                '</div>' +
+                '</div>' +
+                '<div class="container-fluid" ng-show="signIn.showFacebook">' +
+                '<div class="row text-center">' +
+                '<div class="center">' +
+                '<a href="#" ng-click="signIn.fbLogin()"><img ng-src="/images/sign-in-with-facebook.png"/></a>' +
+                '</div>' +
+                '</div>' +
+                '</div>' +
+                '<div class="container-fluid" ng-show="signIn.showManual">' +
+                '<div class="row text-center">' +
+                '<p>Don\'t want to login with Facebook?</p>' +
+                '<form id="signin" action="/signin/authenticate" method="post" class="form-group">' +
+                '<input id="_csrf" name="_csrf" type="hidden" value="{{csrf}}"/>' +
+                '<div class="form-group">' +
+                '<label for="username">Username</label>' +
+                '<input id="username" name="username" type="text" size="25"/>' +
+                '</div>' +
+                '<div class="form-group">' +
+                '<label for="password">Password</label>' +
+                '<input id="password" name="password" type="password" size="25"/>' +
+                '</div>' +
+                '<div class="form-group">' +
+                '<div class="checkbox">' +
+                '<label>' +
+                '<input type="checkbox" id="remember-me" name="remember-me"> Remember me' +
+                '</label>' +
+                '</div>' +
+                '</div>' +
+                '<div class="form-group">' +
+                '<button type="submit" class="btn btn-default">Sign in</button>' +
+                '</div>' +
+                '</form>' +
+                '</div>' +
+                '</div>' +
+                '</div>'
+            );
+            $templateCache.put('views/core-bs/sign-in/signed-in.html', '' +
+                '<div class="row signed-in">' +
+                '<div class="row text-center">' +
+                '<div class="center">' +
+                '<p>Login successful...</p>' +
+                '</div>' +
+                '</div>' +
+                '</div>'
+            );
+        }
+    ]
+);
