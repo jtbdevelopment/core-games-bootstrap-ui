3a5196377f00c33bde6e32bd94784adec0e8702f
JTB <jtbdevelopment@gmail.com>
2018-01-31 20:20:58 -0500

    working on tests

--- src/backdrop/bootstrap-backdrop.component.ts
+++ src/backdrop/bootstrap-backdrop.component.ts
@@ -3,10 +3,10 @@ import {Component} from '@angular/core';
 //  Pretty much a copy of NgbNodalBackdrop - they dont export it so we cant use directly
 @Component({
     selector: 'jtb-modal-backdrop',
-    template: ''//,
-//    host: {
-//        'class': 'modal-backdrop fade show'
-//    }
+    template: '',
+    host: {
+        'class': 'modal-backdrop fade show'
+    }
 })
 export class JTBModalBackdrop {
 }
